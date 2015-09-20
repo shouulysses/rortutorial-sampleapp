d40c9a86583a9db4b364fd575ff96c87bc35bdab
shouulysses <anythinghk@hotmail.com>
2015-09-20 12:52:12 +0000

    Finish Exercise 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar"}
       end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
   end
   
   test "valid signup information" do
@@ -24,5 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "123456"}
       end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
+  
 end

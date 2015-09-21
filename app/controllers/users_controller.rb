9bbca06f8e07737999db54c2b2fc7ccabe8335fd
shouulysses <anythinghk@hotmail.com>
2015-09-21 20:15:03 +0000

    Finish Chapter8 login, logoff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Thank you for signing up"
       redirect_to @user
     else
